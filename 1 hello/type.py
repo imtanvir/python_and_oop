1756877f0a9ed402eba2a81b2ca76a40c006aeca
Md. Tanvir Parvej Badhon <69302248+imtanvir@users.noreply.github.com>
2022-10-09 16:36:53 +0600

    Fix a little bit error of string syntax.
    May be it was a typing mistake

--- 1 hello/type.py
+++ 1 hello/type.py
@@ -14,5 +14,5 @@ print(type(kanamachi))
 person = 'Kodom Ali'
 age = 45
 # about_me = 'My Name is' + person + age
-about_me = f'My Name is {person}. My age is {age}.'
+about_me = 'My Name is {person}. My age is {age}.'
 print(about_me)
